f97ef61616825f703ee9ed21596520e9ea88d5de
Matias <85851263+falduti90@users.noreply.github.com>
2022-11-26 12:44:04 -0300

    Para chechu

---
+++ Modelo de parcial/ModeloChoferes.sql
@@ -0,0 +1,151 @@
+--1) Se pide agregar una modificación a la base de datos para que permita registrar la
+--calificación (de 1 a 10) que el Cliente le otorga al Chofer en un viaje y además una
+--observación opcional. Lo mismo debe poder registrar el Chofer del Cliente.
+--Importante:
+--- No se puede modificar la estructura de la tabla de Viajes.
+--- Sólo se puede realizar una calificación por viaje del Cliente al Chofer.
+--- Sólo se puede realizar una calificación por viaje del Chofer al Cliente.
+--- Puede haber viajes que no registren calificación por parte del Chofer o del
+--Cliente.
+
+CREATE TABLE [dbo].[PuntosChofer] (
+    [ID]               BIGINT  PRIMARY KEY  IDENTITY (1, 1) NOT NULL,
+    [IDViaje]          BIGINT    REFERENCES VIAJES (ID)  NULL, 
+    [Fecha]            DATETIME NOT NULL,
+    [PuntosObtenidos]  INT   CHECK (PuntosObtenidos BETWEEN 0 AND  10 ) NOT NULL,
+    [Observacion]      varchar(200)  NULL,
+    [FechaVencimiento] DATE     NOT NULL
+)
+
+CREATE TABLE [dbo].[PuntosClientes] (
+    [ID]               BIGINT  PRIMARY KEY  IDENTITY (1, 1) NOT NULL,
+    [IDViaje]          BIGINT    REFERENCES VIAJES (ID)  NULL, 
+    [Fecha]            DATETIME NOT NULL,
+    [PuntosObtenidos]  INT   CHECK (PuntosObtenidos BETWEEN 0 AND  10 ) NOT NULL,
+    [Observacion]      varchar(200)  NULL,
+    [FechaVencimiento] DATE     NOT NULL
+)
+
+
+
+
+--2) Realizar una vista llamada VW_ClientesDeudores que permita listar: Apellidos,
+--Nombres, Contacto (indica el email de contacto, si no lo tiene el teléfono y de lo
+--contrario "Sin datos de contacto"), cantidad de viajes totales, cantidad de viajes no
+--abonados y total adeudado. Sólo listar aquellos clientes cuya cantidad de viajes no
+--abonados sea superior a la mitad de viajes totales realizados.ALTER VIEW VW_CLIENTESDEUDORES ASSELECT  FINAL.Apellidos,FINAL.Nombres,FINAL.CONTACTO,FINAL.TOTAL_VIAJES,FINAL.TOTAL_NOPAGOS,FINAL.TOTAL_ADEUDADO FROM (SELECT CL.Apellidos,CL.Nombres ,COALESCE(CL.EMAIL,CL.TELEFONO,'SIN DATOS DE CONTACTO') AS CONTACTO,(	SELECT COUNT(*) FROM VIAJES AS VJ	WHERE VJ.IDCliente= CL.ID AND VJ.Inicio IS NOT NULL) AS 'TOTAL_VIAJES',(		SELECT COUNT(*) FROM VIAJES AS VJ	WHERE VJ.IDCliente= CL.ID AND VJ.Pagado=0 AND VJ.Inicio IS NOT NULL)AS 'TOTAL_NOPAGOS',(	SELECT SUM(VJ.Importe) FROM VIAJES AS VJ	WHERE VJ.IDCliente= CL.ID AND VJ.Pagado=0 AND VJ.Inicio IS NOT NULL)AS 'TOTAL_ADEUDADO'FROM Clientes AS CL)AS FINALWHERE TOTAL_NOPAGOS > (TOTAL_VIAJES/2) SELECT * FROM VW_CLIENTESDEUDORESSELECT COUNT (*) FROM VIAJES  AS VINNER JOIN Clientes AS C ON C.ID=V.IDClienteWHERE C.Apellidos='Tirone'--3) Realizar un procedimiento almacenado llamado SP_ChoferesEfectivo que reciba un
+--año como parámetro y permita lista apellidos y nombres de los choferes que en ese
+--año únicamente realizaron viajes que fueron abonados con la forma de pago
+--'Efectivo'.
+--NOTA: Es indistinto si el viaje fue pagado o no. Utilizar la fecha de inicio del viaje para
+--determinar el año del mismo
+
+ALTER PROC SP_CHOFERESEFECTIVO (
+	@INICIO INT
+) 
+AS
+BEGIN
+
+SELECT FINAL.Apellidos,FINAL.Nombres FROM (
+	SELECT CH.Apellidos,CH.Nombres,
+	(
+		SELECT  COUNT (*) FROM VIAJES AS VJ
+		INNER JOIN FormasPago AS FP  ON VJ.FormaPago=FP.ID
+		WHERE YEAR(VJ.Inicio)=@INICIO AND FP.Nombre ='Efectivo' AND CH.ID= VJ.IDChofer 
+	
+		
+	) AS 'TOTAL_EFECTIVO',
+	
+	(
+		SELECT  COUNT (*) FROM VIAJES AS VJ
+		INNER JOIN FormasPago AS FP  ON VJ.FormaPago=FP.ID
+		WHERE YEAR(VJ.Inicio)=@INICIO AND FP.Nombre <> 'Efectivo' AND CH.ID= VJ.IDChofer 
+	
+
+	)AS 'TOTAL_OTROS'
+	FROM CHOFERES AS CH
+
+) AS  FINAL
+WHERE TOTAL_EFECTIVO>0 AND TOTAL_OTROS=0
+END
+
+EXEC SP_CHOFERESEFECTIVO 2021
+
+SELECT * FROM Choferes  C
+JOIN Viajes  V ON V.IDChofer=C.ID
+JOIN FormasPago F ON F.ID=V.FormaPago
+WHERE V.Inicio IS NOT NULL
+
+
+CREATE PROC SP_BRIAN (
+@anio int
+)
+as
+select c.id, c.Apellidos, c.Nombres
+from Choferes as c,
+(select v.IDChofer, count(*) cantidad from Viajes v inner join FormasPago f on v.FormaPago = f.ID 
+    where year(v.Inicio) = @anio and f.Nombre = 'Efectivo' group by v.IDChofer) cantEfectivo,
+(select v1.IDChofer, count(*) cantidad from Viajes v1 inner join FormasPago f on v1.FormaPago = f.ID 
+    where year(v1.Inicio) = @anio and f.Nombre <> 'Efectivo' group by v1.IDChofer) cantDistintoEfectivo
+where c.ID = cantEfectivo.IDChofer and c.ID = cantDistintoEfectivo.IDChofer and cantEfectivo.cantidad >0
+and cantDistintoEfectivo.cantidad = 0
+
+exec SP_BRIAN 2021
+
+--4) Realizar un trigger que al borrar un cliente, primero quitarle todos los puntos (baja
+--física) y establecer a NULL todos los viajes de ese cliente. Luego, eliminar
+--físicamente el cliente de la base de datos
+
+ALTER TRIGGER TR_ELIMINAR_CLIENTE ON CLIENTES
+AFTER DELETE
+AS
+BEGIN
+	BEGIN TRY
+			BEGIN TRANSACTION -- USO TRANSACCIO YA QUE SON  3  CONSULTA DE ACCION QUE SE EJECUTAN
+			DECLARE @IDCLIENTE BIGINT
+			SELECT @IDCLIENTE= D.ID FROM deleted AS D
+
+			--ELIMINO TODO LOS REGISTROS  DE LA TABLA DE PUNTOS
+
+			DELETE FROM PUNTOS WHERE  IDCliente=@IDCLIENTE
+			-- CAMBIO A NULL EL IDCLIENTE DE LA TABLA VIAJES
+			UPDATE VIAJES SET IDCliente= NULL WHERE IDCliente=@IDCLIENTE
+			--ELIMINO EL CLIENTE DE LA TABLA CLIENTES
+			DELETE CLIENTES WHERE ID=@IDCLIENTE
+
+			COMMIT TRANSACTION
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK
+	END CATCH
+
+END
+
+--5) Realizar un trigger que garantice que el Cliente sólo pueda calificar al Chofer si el
+--viaje se encuentra pagado. Caso contrario indicarlo con un mensaje aclaratorio.
+
+CREATE TRIGGER TR_VALIDAR_CALIACION ON [PuntosChofer]
+AFTER INSERT
+AS
+BEGIN
+
+		DECLARE @ID_VIAJE BIGINT
+		DECLARE @PAGO BIT
+
+		SELECT @ID_VIAJE=IDViaje FROM inserted
+		SELECT @PAGO=Pagado FROM Viajes WHERE ID=@ID_VIAJE
+
+		IF(@PAGO=0)
+		BEGIN
+			ROLLBACK TRANSACTION
+				PRINT('----------No se puedo calificar el chofer, por falta de pago----------------')
+		END
+
+		ELSE
+		BEGIN
+			PRINT('----------Gracias por calificar el viaje----------------')
+		END
+
+
+END
---
+++ Modelo de parcial/SQLQuery1.sql
@@ -0,0 +1,173 @@
+USE ExamenIntegrador20222C
+
+-- Parcial: Integrador SQL LaboratorioIII
+-- Alumno:  Matias Nicolas Falduti
+-- Legajo:  24495
+
+select * from Concursos
+SELECT * FROM FOTOGRAFIAS
+SELECT* FROM Participantes
+SELECT * FROM VOTACIONES
+-- 1)Hacer un procedimiento almacenado llamado SP_Ranking que a partir de un IDParticipante 
+--se pueda obtener las tres mejores fotografías publicadas (si las hay).
+--Indicando el nombre del concurso, apellido y nombres del participante, 
+--el título de la publicación, la fecha de publicación y el puntaje promedio obtenido por esa publicación.
+CREATE PROC SP_RANKING (
+@IDPARTICIPANTE BIGINT
+)
+AS
+BEGIN
+SELECT TOP 3 C.Titulo,P.Apellidos,P.Nombres,F.Titulo,F.Publicacion,
+(
+	SELECT AVG(V.Puntaje) FROM Votaciones AS V
+	WHERE F.ID= V.IDFotografia
+) AS PROMEDIO
+FROM  Concursos AS C
+INNER JOIN Fotografias AS F ON F.IDConcurso= C.ID
+INNER JOIN Participantes AS P ON P.ID= F.IDParticipante
+WHERE P.ID=@IDPARTICIPANTE
+ORDER BY PROMEDIO DESC
+END
+
+EXEC SP_RANKING 2
+--------------------------------------------------------------------------------------------------------------------
+--2Hacer un procedimiento almacenado llamado SP_Descalificar que reciba un ID de fotografía 
+--y realice la descalificación de la misma. También debe eliminar todas las votaciones registradas 
+--a la fotografía en cuestión. Sólo se puede descalificar una fotografía si pertenece a un concurso no finalizado.
+
+
+CREATE PROC SP_DESCALIFICAR (
+@IDFOTOGRAFIA BIGINT
+)
+AS 
+BEGIN
+	DECLARE @FIN DATE,@CONCURSO BIGINT,@PUBLICACION DATE
+	SELECT @CONCURSO = IDConcurso,@PUBLICACION=Publicacion FROM Fotografias AS F
+	WHERE F.ID=@IDFOTOGRAFIA
+	SELECT @FIN =Fin FROM Concursos AS C
+	WHERE C.Fin =@CONCURSO
+
+	IF(@PUBLICACION<@FIN)
+	BEGIN
+		UPDATE Fotografias SET Descalificada=1 WHERE @IDFOTOGRAFIA=ID
+
+		DELETE FROM  Votaciones 
+		WHERE @IDFOTOGRAFIA= IDFotografia
+	END
+
+	ELSE
+	BEGIN
+		PRINT('')
+	END
+
+END
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------
+--3Al insertar una fotografía verificar que el usuario creador de la fotografía tenga el
+--ranking suficiente para participar en el concurso. También se debe verificar que el concurso haya iniciado y 
+--no finalizado. Además, el participante no debe registrar una descalificación en los últimos 100 días. 
+--Si ocurriese un error, mostrarlo con un mensaje aclaratorio. De lo contrario, 
+--insertar el registro teniendo en cuenta que la fecha de publicación es la fecha y hora del sistema.
+CREATE TRIGGER TR_FOTOGRAFIA ON FOTOGRAFIAS
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @IDFOTO BIGINT
+	DECLARE @IDPARTICPARTE BIGINT
+	DECLARE @IDCONCURSO BIGINT 
+	DECLARE @RANKING DECIMAL(5,2)
+	DECLARE @RANKING_MIN DECIMAL(5,2)
+	DECLARE @FEC_INI DATE
+	DECLARE @FEC_FIN DATE
+	DECLARE @ULTIMA_FECHA DATE
+
+	SELECT @IDPARTICPARTE= IDParticipante, @IDCONCURSO=IDConcurso FROM inserted
+		
+
+	SELECT @RANKING= AVG (Puntaje) FROM Votaciones AS V
+	INNER JOIN Fotografias AS F ON F.ID= V.IDFotografia
+	INNER JOIN Participantes AS P ON P.ID= F.IDParticipante
+	WHERE P.ID=IDParticipante
+
+	SELECT @RANKING_MIN= RankingMinimo , @FEC_INI=Inicio, @FEC_FIN= FIN FROM Concursos AS C
+	WHERE C.ID=@IDCONCURSO
+
+	SELECT  @ULTIMA_FECHA = MAX (C.Fin) FROM Fotografias AS F
+	INNER JOIN Concursos AS C ON C.ID= F.IDConcurso
+	WHERE @IDPARTICPARTE= F.IDParticipante
+
+	IF(@RANKING < @RANKING_MIN OR @FEC_INI>GETDATE()OR  @FEC_FIN < GETDATE()  OR DATEDIFF(DAY,@ULTIMA_FECHA,GETDATE())<100  )
+	BEGIN
+		ROLLBACK TRANSACTION
+		PRINT ('NO SE PUDO INGRESAR  LA FOTO')
+	END
+
+
+
+END
+
+
+
+CREATE VIEW VW_DESCALIFICADOS  AS
+SELECT F.IDParticipante, C.Fin  FROM Fotografias AS F
+INNER JOIN Concursos AS C ON C.ID= F.IDConcurso
+WHERE F.Descalificada = 1
+
+-----------------------------------------------------------------------------------------------------------------
+--4)Al insertar una votación, verificar que el usuario que vota no lo haga más de una vez para el mismo concurso ni se 
+--pueda votar a sí mismo. Tampoco puede votar una fotografía descalificada.
+
+CREATE TRIGGER TR_VALIDACIONES ON VOTACIONES
+INSTEAD OF INSERT
+AS 
+BEGIN 
+
+		
+		DECLARE @IDPARTICIPANTE BIGINT 
+		DECLARE @IDFOTOGRAFIA BIGINT
+		DECLARE @IDVOTANTE BIGINT
+		DECLARE @DESCALIFICADA BIT
+		DECLARE @CANT_VOTACIONES INT
+		DECLARE @IDCONCURSO BIGINT
+
+		SELECT @IDFOTOGRAFIA=IDFotografia,@IDVOTANTE=IDVotante FROM Inserted
+
+		SELECT  @IDPARTICIPANTE=FOT.IDParticipante,@DESCALIFICADA=FOT.Descalificada,@IDCONCURSO=FOT.IDConcurso FROM FOTOGRAFIAS AS FOT
+
+		SELECT @CANT_VOTACIONES= COUNT (*) FROM Votaciones V 
+		INNER JOIN FOTOGRAFIAS AS F ON F.ID= V.IDFotografia
+		WHERE V.IDVotante=@IDVOTANTE AND  F.IDConcurso=@IDCONCURSO
+
+		IF (@CANT_VOTACIONES<2 AND @IDVOTANTE<>@IDPARTICIPANTE AND @DESCALIFICADA=0)
+		BEGIN
+			INSERT INTO VOTACIONES (IDVotante, IDFotografia, Fecha, Puntaje )
+			SELECT IDVotante, IDFotografia, Fecha, Puntaje FROM Inserted			
+		END 
+
+		ELSE
+		BEGIN
+		RAISERROR ('NO SE PUDO COMPLETAR LA VOTACION',16,1)
+		END
+END
+
+
+
+
+
+
+-------------------------------------------------------------------------------------------
+
+--5)Hacer un listado en el que se obtenga: ID de participante, apellidos y nombres de los participantes que hayan 
+--registrado al menos dos fotografías descalificadas.
+SELECT FINAL.ID,FINAL.Apellidos,FINAL.Nombres FROM (
+SELECT P.ID,P.Apellidos,P.Nombres ,
+(
+ SELECT COUNT (*) FROM Fotografias AS F
+ WHERE F.IDParticipante = P.ID AND F.Descalificada=1
+ ) AS'TOTAL_DESCALIFICADAS'
+FROM Participantes AS P
+)AS FINAL
+WHERE TOTAL_DESCALIFICADAS>=2
